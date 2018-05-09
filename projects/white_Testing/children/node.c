b56cd8a77accd7df5bc8072ee0670388dede97ab
LW-Ho <leoleo41306@gmail.com>
2018-05-09 14:14:04 +0800

    no message

--- projects/white_Testing/children/node.c
+++ projects/white_Testing/children/node.c
@@ -63,6 +63,7 @@
 #define PRINTF(...) printf(__VA_ARGS__)
 #define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
 #define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]", (lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3], (lladdr)->addr[4], (lladdr)->addr[5])
+#include "dev/sht21.h"  //temporaly
 #else
 #define PRINTF(...)
 #define PRINT6ADDR(addr)
@@ -70,7 +71,7 @@
 #endif
 
 #include "dev/leds.h"
-//#include "dev/sht21.h"  //temporaly
+
 //#include "dev/max44009.h"  //light
 
 extern resource_t res_hello, res_push, res_toggle, res_collect, res_bcollect; // , res_temperature;
