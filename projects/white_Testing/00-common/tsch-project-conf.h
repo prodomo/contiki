266d82355f8abb0f77884d3535f57009e08d2778
LookLikeAName <yl301l4ul6@gmail.com>
2018-07-18 11:10:08 +0800

    set reasonable slotframe period

--- projects/white_Testing/00-common/tsch-project-conf.h
+++ projects/white_Testing/00-common/tsch-project-conf.h
@@ -143,9 +143,9 @@
 #define NETSTACK_CONF_ROUTING_NEIGHBOR_REMOVED_CALLBACK orchestra_callback_child_removed
 
 /* Dimensioning */
-#define ORCHESTRA_CONF_EBSF_PERIOD                     29
+#define ORCHESTRA_CONF_EBSF_PERIOD                     51
 #define ORCHESTRA_CONF_COMMON_SHARED_PERIOD            11 /* Common shared slot, 7 is a very short slotframe (high energy, high capacity). Must be prime and at least equal to number of nodes (incl. BR) */
-#define ORCHESTRA_CONF_UNICAST_PERIOD                  11 /* First prime greater than 10 */
+#define ORCHESTRA_CONF_UNICAST_PERIOD                  13 /* First prime greater than 10 */
 
 #endif /* WITH_ORCHESTRA */
 
