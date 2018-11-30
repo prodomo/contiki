f36f199fce46fed8a4cb03ce037cbb04892a72c3
LW-Ho <leoleo41306@gmail.com>
2018-11-30 13:05:03 +0800

    Enable rpl debug messages.

--- apps/er-coap/er-coap-engine.c
+++ apps/er-coap/er-coap-engine.c
@@ -42,7 +42,7 @@
 #include <string.h>
 #include "er-coap-engine.h"
 
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
--- apps/er-coap/er-coap-observe.c
+++ apps/er-coap/er-coap-observe.c
@@ -40,7 +40,7 @@
 #include <string.h>
 #include "er-coap-observe.h"
 
-#define DEBUG 1
+#define DEBUG 0
 #if DEBUG
 #include <stdio.h>
 #define PRINTF(...) printf(__VA_ARGS__)
