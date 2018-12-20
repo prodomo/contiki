24ea964408d6ac9fcaf92ba8d4eea7de5d2b33a9
LW-Ho <leoleo41306@gmail.com>
2018-12-20 20:25:44 +0800

    add random number plus to observe refresh intervel.

--- apps/er-coap/er-coap-observe.c
+++ apps/er-coap/er-coap-observe.c
@@ -40,6 +40,8 @@
 #include <string.h>
 #include "er-coap-observe.h"
 
+#include "lib/random.h"
+
 #define DEBUG 0
 #if DEBUG
 #include <stdio.h>
@@ -195,6 +197,9 @@ coap_notify_observers_sub(resource_t *resource, const char *subpath)
   int url_len, obs_url_len;
   char url[COAP_OBSERVER_URL_LEN];
 
+  random_init(0);
+  int r = (random_rand() % 10) +1;
+
   url_len = strlen(resource->url);
   strncpy(url, resource->url, COAP_OBSERVER_URL_LEN - 1);
   if(url_len < COAP_OBSERVER_URL_LEN - 1 && subpath != NULL) {
@@ -229,7 +234,7 @@ coap_notify_observers_sub(resource_t *resource, const char *subpath)
       
       if((transaction = coap_new_transaction(coap_get_mid(), &obs->addr, obs->port))) {
 
-        if(obs->obs_counter % (COAP_OBSERVE_REFRESH_INTERVAL) == 0) {
+        if(obs->obs_counter % (COAP_OBSERVE_REFRESH_INTERVAL+r) == 0) {
           PRINTF("           Force Confirmable for\n");
           notification->type = COAP_TYPE_CON;
         }
